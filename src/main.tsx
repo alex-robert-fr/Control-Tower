a3d57544dd0ba41ff773a634f7c7811fdbf67bdc
Alex Robert <contact.alexrobert04@gmail.com>
2023-12-18 12:39:58 +0100

    REFACTOR: Delete old version

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -17,7 +16,6 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
       <App_v2 />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
